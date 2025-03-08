11d82a2a8a591742297a43d16a70e619b1b91e69
Malihashahi <maliha.shahi.03@gmail.com>
2025-03-08 22:30:18 +0330

    add axios for getting users

--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from './pages/HomeComponent.vue';
+import IndexUser from './pages/users/user_index.vue';
 
 const routes = [
-    { path: "/", name: "home", component: Home }
+    { path: "/", name: "home", component: Home },
+    { path: "/users", name: "users", component: IndexUser }
 ];
 
 const router = createRouter({
