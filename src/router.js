31fe7f46c1c552ddbea8335e3830478957d5dbdb
Malihashahi <maliha.shahi.03@gmail.com>
2025-03-12 23:05:48 +0330

    show post with router view and and template for post

--- src/router.js
+++ src/router.js
@@ -1,15 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from './pages/HomeComponent.vue';
+
 import TemplateUser from './pages/users/UserTemplate.vue';
 import IndexUser from './pages/users/user_index.vue';
 import ShowUser from './pages/users/ShowUser.vue';
 
+import TemplatePost from './pages/posts/PostTemplate.vue';
+import IndexPost from './pages/posts/PostIndex.vue';
+
 const routes = [
     { path: '/', name: 'home', component: Home },
-    { path: '/users', name: 'userTemplate', component: TemplateUser , children:[
-        {path: '', name: 'users', component: IndexUser},
-        { path: ':id', name: 'userId', component: ShowUser }
-    ] }
+    {
+        path: '/users', name: 'userTemplate', component: TemplateUser, children: [
+            { path: '', name: 'users', component: IndexUser },
+            { path: ':id', name: 'userId', component: ShowUser }
+        ]
+    },
+    {
+        path: '/posts', name: 'postTemplate', component: TemplatePost, children: [
+            { path: '', name: 'posts', component: IndexPost },
+            // { path: ':id', name: 'postId', component: ShowPost }
+        ]
+    }
 ];
 
 const router = createRouter({
