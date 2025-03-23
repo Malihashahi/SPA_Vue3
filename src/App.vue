88bc1ab20e6a1211b3702ed2687aa7413f7e7709
Malihashahi <maliha.shahi.03@gmail.com>
2025-03-23 23:10:38 +0430

    reactivty in vue

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,25 @@
   <div>
     <Header />
     <router-view></router-view>
+    <h2>{{ count }} - {{ title }}</h2>
  
   </div>
 </template>
 
 <script setup>
 import Header from './components/sections/HeaderSection.vue'
-
+import {ref,watch } from 'vue';
 
 
     Header
+function trackChange(x) {
+   watch(x, (x) => {
+    console.log('x changed!')
+  })
+}
+
+let number = $ref(0);
+trackChange($$(number));
  
 </script>
 
